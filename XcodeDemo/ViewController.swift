562227a281ce44e3d7ef105f5acdc9d4b8db22e1
Jonathan E. Hardis <jhardis@me.com>
2021-09-14 19:10:35 -0400

    Trying to make the thing work.

---
+++ XcodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeDemo
+//
+//  Created by Jonathan E. Hardis on 9/14/21.
+//  Copyright © 2021 Jonathan E. Hardis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
